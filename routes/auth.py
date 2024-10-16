1e902490a02a97277fe2ab9d8df077dc0889e26a
matiasdev30 <matiasdev30@gmail.com>
2024-10-16 16:40:53 +0100

    feat: remove route /home from auth.py to web.py

--- routes/auth.py
+++ routes/auth.py
@@ -1,12 +1,10 @@
 from fastapi.responses import HTMLResponse, RedirectResponse
 from .config import router, templates
 from fastapi import Form, Request
+import os
+
 
 @router.get("/login", response_class=HTMLResponse)
 async def read_user(request: Request):
     return templates.TemplateResponse("login.html", {"request" : request})
 
-@router.post("/home")
-async def home(request: Request, email: str = Form(...)):
-    return templates.TemplateResponse("home.html", {"request": request, "email" : email})
-    #return {"message": "Bem-vindo à página inicial!"}
