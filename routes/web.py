526f5d3f3e7809897f016ad478202dd3797705f2
matiasdev30 <matiasdev30@gmail.com>
2024-10-16 11:34:43 +0100

    feat: add login route

--- routes/web.py
+++ routes/web.py
@@ -7,6 +7,6 @@ async def hello(request: Request):
     return templates.TemplateResponse("index.html", {"request": request, "message" : "Olá, Ninja templates com FastAPI"}) 
 
 
-@router.get("/user/{username}", response_class=HTMLResponse)
-async def read_user(request: Request, username: str):
-    return templates.TemplateResponse("user.html", {"request" : request, "username": username})
+@router.get("/login", response_class=HTMLResponse)
+async def read_user(request: Request):
+    return templates.TemplateResponse("login.html", {"request" : request})
