db85af88894ef289019ea8ce1baf09a03b2760a0
matiasdev30 <matiasdev30@gmail.com>
2024-10-16 12:08:26 +0100

    feta: include auth router

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 from routes.web import router as web_router
+from routes.auth import router as auth_router
 
 app = FastAPI()
 
@@ -8,3 +9,4 @@ app.mount("/static", StaticFiles(directory="static"), name="static")
 
 #incluindo as rotas
 app.include_router(web_router)
+app.include_router(auth_router)
