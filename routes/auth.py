1f53e7a8fd3a8606b91c0f820de226c61f32588b
matiasdev30 <matiasdev30@gmail.com>
2024-10-16 12:09:20 +0100

    create lib for auth module for authentication

--- routes/auth.py
+++ routes/auth.py
@@ -1,6 +1,12 @@
+from fastapi.responses import HTMLResponse, RedirectResponse
 from .config import router, templates
-from fastapi import Request
+from fastapi import Form, Request
 
-@router.get("/login")
-async def login(request: Request):
-    return templates.TemplateResponse(name="login.html", request={"request" : request, "message" : "login page"})
+@router.get("/login", response_class=HTMLResponse)
+async def read_user(request: Request):
+    return templates.TemplateResponse("login.html", {"request" : request})
+
+@router.post("/home")
+async def home(request: Request, email: str = Form(...)):
+    return templates.TemplateResponse("home.html", {"request": request, "email" : email})
+    #return {"message": "Bem-vindo à página inicial!"}
